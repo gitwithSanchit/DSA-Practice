ff7ed668d3f988d845b3077bf47509cc7c8f6648
Sanchit gupta <sanchitg7237@gmail.com>
2025-11-14 14:45:35 +0530

    Solved MergeSortedArray

--- src/Sorting/MergeSortedArray.java
+++ src/Sorting/MergeSortedArray.java
@@ -1,7 +1,37 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class MergeSortedArray {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        System.out.println("DSA practice");
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;       // pointer for nums1
+        int j = n - 1;       // pointer for nums2
+        int k = m + n - 1;   // pointer for final position in nums1
+
+        // while both arrays have elements
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // if nums2 still has elements left
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
     }
 }
