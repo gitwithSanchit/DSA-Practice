1b93df9681868aa02d01e3b81631c7bf2c9f0fef
Sanchit gupta <sanchitg7237@gmail.com>
2025-11-14 14:27:17 +0530

    First commit

---
+++ src/Sorting/MergeSortedArray.java
@@ -0,0 +1,9 @@
+package Sorting;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.out.println("DSA practice");
+    }
+}
